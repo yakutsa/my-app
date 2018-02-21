a10d6c43578733cd2bcecd37067342799a73c872
Savas Yakut <savas.yakut@gmail.com>
2018-02-21 21:32:14 +0100

    styling elements with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,10 +6,18 @@ import { Component } from '@angular/core';
 })
 export class ServerComponent {
   serverId: number = 10;
-  serverStatus: string = "offline";
+  serverStatus: string = 'offline';
+
+  constructor(){
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   getserverStatus(){
     return this.serverStatus;
+  }
+
+  getColor(){
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 
 }
